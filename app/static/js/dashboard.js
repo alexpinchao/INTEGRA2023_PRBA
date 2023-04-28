d673e1d0ebc7a2fb71f86e406bebeeeb4461b106
MateoBarrera <materile1997@gmail.com>
2023-04-28 10:53:51 -0500

    feat macros: generación de macros con frontend preliminar

--- app/static/js/dashboard.js
+++ app/static/js/dashboard.js
@@ -1,11 +1,19 @@
-$( document ).ready( function() {
-	$(".form-range").each(function(){
-		$(this).bootstrapSlider();
-		$(this).on("change", function(slideEvt) {
+$(document).ready(function () {
+	$(".form-range").each(function () {
+		$(this).bootstrapSlider()
+		$(this).on("change", function (slideEvt) {
 			console.log(slideEvt)
-			$(this).parents(".group-form-range").find(".form-range-value").text(slideEvt.value.newValue);
-		});
-		
-	});
-	
- } );
+			$(this).parents(".group-form-range").find(".form-range-value").text(slideEvt.value.newValue)
+			updateChart()
+		})
+	})
+	addClass()
+})
+
+function addClass() {
+	document.getElementsByClassName("slider").forEach((element) => {
+		element.classList.add("slider-volt")
+	})
+}
+
+function updateChart() {}
